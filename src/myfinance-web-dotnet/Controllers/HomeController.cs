97212ac1bde46120c31f65a2d54bcfa5da57dac3
nathaliaassis <nathaliamassis@gmail.com>
2023-02-07 22:28:09 -0300

    home model/controller/index

--- src/myfinance-web-dotnet/Controllers/HomeController.cs
+++ src/myfinance-web-dotnet/Controllers/HomeController.cs
@@ -6,26 +6,32 @@ namespace myfinance_web_dotnet.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+  private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+  public HomeController(ILogger<HomeController> logger)
+  {
+    _logger = logger;
+  }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+  public IActionResult Cadastro(HomeModel model)
+  {
+    var teste = model.Nome;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    return RedirectToAction("Index");
+  }
+  public IActionResult Index()
+  {
+    return View();
+  }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+  public IActionResult Privacy()
+  {
+    return View();
+  }
+
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  public IActionResult Error()
+  {
+    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+  }
 }
---
+++ src/myfinance-web-dotnet/Models/HomeModel.cs
@@ -0,0 +1,7 @@
+namespace myfinance_web_dotnet.Models
+{
+  public class HomeModel
+  {
+    public string? Nome { get; set; }
+  }
+}
